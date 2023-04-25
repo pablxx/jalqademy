770e03f0f1817d96ee15a3812ae13614a0d47a52
pabloUSYIT <psaavedra@usyitstudios.com>
2023-04-25 17:26:35 -0400

    configs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-
+module.exports = {
+    images: {
+        unoptimized: true,
+      },
+}
 
 module.exports = (phase) => {
+
     if (phase === PHASE_DEVELOPMENT_SERVER) {
         return {
             env: {
